2c30dbb4c7ae2f9abea6aac0dfa9cfd19f60ca2c
Nguyen Dinh Duy <nguyendinhduybigtreetc@gmail.com>
2023-10-17 15:54:26 +0700

    BOK00001: Spring security config base, user login

---
+++ src/main/java/com/bokint/datawarehouse/model/res/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.bokint.datawarehouse.model.res;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+    private String jwtToken;
+    private String username;
+}
